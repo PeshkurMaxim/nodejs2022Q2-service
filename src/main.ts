39301850a89a6112a28ab9e0e9325193abbabcf1
Пешкур Максим <m.peshkur@ostrovchistoty.by>
2023-02-03 14:26:30 +0300

    create openApi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config as dotenvConfig } from 'dotenv';
+import { SwaggerModule } from '@nestjs/swagger';
+import { getDocument } from './openapi';
 
 dotenvConfig();
 
@@ -10,6 +12,9 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const document = await getDocument();
+  SwaggerModule.setup('doc', app, document);
   await app.listen(PORT);
 }
 bootstrap();
