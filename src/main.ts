7a9ecde8320ad5ff49e333445bcd868dbee96124
Пешкур Максим <m.peshkur@ostrovchistoty.by>
2023-02-22 10:02:30 +0300

    add error handling

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { AppModule } from './app.module';
 import { config as dotenvConfig } from 'dotenv';
 import { SwaggerModule } from '@nestjs/swagger';
 import { getDocument } from './openapi';
+import { AllExceptionFilter } from './common/exceptionsFilters/all-exception.filter';
+
+process.on('uncaughtException', (err) => {
+  // logger
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  //logger
+});
 
 dotenvConfig();
 
@@ -11,11 +21,14 @@ async function bootstrap() {
   const PORT = process.env.PORT || 4000;
 
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  app.useGlobalFilters(new AllExceptionFilter());
 
   const document = await getDocument();
   SwaggerModule.setup('doc', app, document);
