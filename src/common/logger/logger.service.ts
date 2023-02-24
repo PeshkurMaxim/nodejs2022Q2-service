bc67061ac1f3d50d8bbfb0511bb83d3867f0878b
= <=>
2023-02-25 01:13:47 +0300

    refactor logger

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -21,10 +21,8 @@ export class CustomLoggerService extends ConsoleLogger {
     super();
 
     const SERVER_ROOT_DIR = this.configService.get<string>('SERVER_ROOT_DIR');
-    const loggerLevel =
-      process.env.LOGGER_LEVEL in LogLevel ? process.env.LOGGER_LEVEL : 'log';
-
-    this.loggerLevel = LogLevel[loggerLevel];
+    this.loggerLevel =
+      process.env.LOGGER_LEVEL in LogLevel ? +process.env.LOGGER_LEVEL : 3;
 
     this.logFile = createWriteStream(join(SERVER_ROOT_DIR, '/logs/app.log'), {
       flags: 'a+',
@@ -39,38 +37,46 @@ export class CustomLoggerService extends ConsoleLogger {
   }
 
   error(message: string, ...optionalParams: any[]) {
+    if (LogLevel.error > this.loggerLevel) return;
+
     this.writeLog(LogLevel.error, message);
     if (optionalParams.length == 0) super.error(message);
     else super.error(message, optionalParams);
   }
 
   warn(message: string, ...optionalParams: any[]) {
+    if (LogLevel.warn > this.loggerLevel) return;
+
     this.writeLog(LogLevel.warn, message);
     if (optionalParams.length == 0) super.warn(message);
     else super.warn(message, optionalParams);
   }
 
   log(message: string, ...optionalParams: any[]) {
+    if (LogLevel.log > this.loggerLevel) return;
+
     this.writeLog(LogLevel.log, message);
     if (optionalParams.length == 0) super.log(message);
     else super.log(message, optionalParams);
   }
 
   verbose(message: string, ...optionalParams: any[]) {
+    if (LogLevel.verbose > this.loggerLevel) return;
+
     this.writeLog(LogLevel.verbose, message);
     if (optionalParams.length == 0) super.verbose(message);
     else super.verbose(message, optionalParams);
   }
 
   debug(message: string, ...optionalParams: any[]) {
+    if (LogLevel.debug > this.loggerLevel) return;
+
     this.writeLog(LogLevel.debug, message);
     if (optionalParams.length == 0) super.debug(message);
     else super.debug(message, optionalParams);
   }
 
   private writeLog(level: number, message: string) {
-    if (level > this.loggerLevel) return;
-
     const formatedMessage = super.getTimestamp() + ' - ' + message + '\n';
 
     this.writeToFile(this.logFile, formatedMessage);
